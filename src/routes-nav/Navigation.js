07abf2644970b1bfffc9d2af4bc7b536956db069
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-06-22 13:35:05 -0500

    Update nav component to reflect logged in/out status

--- src/routes-nav/Navigation.js
+++ src/routes-nav/Navigation.js
@@ -1,25 +1,69 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
+import UserContext from "../auth/UserContext";
 
 /**
  * Nav bar that shows up on top of every page
+ * If the user is logged in, will show links to 
+ * jobs/companies/profile. Otherwise, will show 
+ * login/signup links. 
  */
 
-function Navigation() {
+function Navigation({ logout }) {
+
+  const { currentUser } = useContext(UserContext);
+
+  function loggedInNav() {
+    return (
+      <ul>
+        <li>
+          <NavLink to="/companies">
+            companies
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/jobs">
+            Jobs
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/profile">
+            profile
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/" onClick={logout}>
+            Logout 
+          </NavLink>
+        </li>
+      </ul>
+    );
+  }
+
+  function loggedOutNav() {
+    return (
+      <ul>
+        <li>
+          <NavLink to="/login">
+            Login 
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/signup">
+            Signup 
+          </NavLink>
+        </li>        
+      </ul>
+    );
+  }
+
   return (
-    <ul>
-      <li>
-        <NavLink to="/companies">
-          companies
-        </NavLink>
-        <NavLink to="/jobs">
-          Jobs
-        </NavLink>
-        <NavLink to="/profile">
-          profile
-        </NavLink>
-      </li>
-    </ul>
+    <nav>
+      <Link to="/">
+        Jobly 
+      </Link>
+      {currentUser ? loggedInNav() : loggedOutNav()}
+    </nav>
   );
 }
 
